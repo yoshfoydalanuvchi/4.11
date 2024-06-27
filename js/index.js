6acf7f8e8f6bded8b9fe8371090a6b3fc787173b
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-27 11:13:57 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// Obyektlar aloqador masalalar.
+
+const person = {
+  name: "Muhammmadrasul",
+  age: 16,
+  job: "developer",
+};
+// console.log(person); //1
+
+// person.name = "Vali";
+// console.log(person); //2
+
+// person.addres = "Fergana";
+// console.log(person); //3
+
+// delete person.age;
+// console.log(person); //4
+
+// person.tel = { mobil: +998339800001, home: +998916695533 };
+// console.log(person); //5
+
+// for (const key in person) {
+//   console.log(person[key]); //6
+// }
+
+// console.log(person.job); //7
+
+// function personObject(name, age, job) {
+//   const person = {
+//     name,
+//     age,
+//     job,
+//   };
+//   return person;
+// }
+// console.log(personObject("Vali", 20, "Enginer")); //8
+
+// Tugadi
